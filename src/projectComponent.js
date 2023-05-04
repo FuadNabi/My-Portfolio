f0b8b165530006792a9a6bea5d1b7d35648e1efc
FuadNabi <fuad.nebiyev95@mail.ru>
2023-05-04 17:14:57 +0400

    Add all projects

--- src/projectComponent.js
+++ src/projectComponent.js
@@ -11,16 +11,18 @@ function projectTechnologies(technologies) {
 export default (projectData) => `
     <div class="project-cont project-number-${projectData.id}">
       <img src="${projectData.image}" alt="Project picture" class="project-img">
-      <div class="project-info">
-        <h3 class="project-name">
-          ${projectData.name}
-        </h3>
-        <ul class="project-languages">
-          ${projectTechnologies(projectData.technologies)}
-        </ul>
-        <button data-project-id=${projectData.id} class="project-btn">
-          See this project <span class="project-btn-icon"><i class="fa-solid fa-arrow-right-long"></i></span>
-        </button>
+      <div class="project-info-width">
+        <div class="project-info">
+          <h3 class="project-name">
+            ${projectData.name}
+          </h3>
+          <ul class="project-languages">
+            ${projectTechnologies(projectData.technologies)}
+          </ul>
+          <button data-project-id=${projectData.id} class="project-btn">
+            See this project <span class="project-btn-icon"><i class="fa-solid fa-arrow-right-long"></i></span>
+          </button>
+        </div>  
       </div>
     </div>
   `;
